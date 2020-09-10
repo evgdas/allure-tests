32f71e6c173585320599a6eb6204e04e1733a95b
Evgeniy D <evg.das@gmail.com>
2020-09-10 15:40:35 +0200

    update

--- src/main/java/utils/helpers/EnvironmentHelper.java
+++ src/main/java/utils/helpers/EnvironmentHelper.java
@@ -6,8 +6,8 @@ public class EnvironmentHelper {
             browser = System.getProperty("browser", "chrome"),
             version = System.getProperty("version"),
             driverFromFile = System.getProperty("driver"),
-            remoteDriverUrl = System.getProperty("remote_driver_url"), // https://username:password@selenoid.autotests.cloud:4444/wd/hub/
-            videoStorageUrl = System.getProperty("video_storage_url"); // https://selenoid.autotests.cloud/video/
+            remoteDriverUrl = System.getProperty("remote_driver_url"),
+            videoStorageUrl = System.getProperty("video_storage_url");
 
 
     public static final boolean
